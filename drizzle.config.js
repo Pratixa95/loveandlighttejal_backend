55d15f61ebbbe15f14cf8554f5873bbcfaeeb9ea
Pratixa95 <mahetapratixa7695@gmail.com>
2026-03-09 14:30:44 +0530

    DB synced with Render (final fix)

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,10 +1,15 @@
 import "dotenv/config";
 
 export default {
-  schema: "./src/db/schema/index.js",
+  schema: "./src/db/schema",
   out: "./drizzle",
+
   dialect: "postgresql",
+
   dbCredentials: {
     url: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   },
 };
