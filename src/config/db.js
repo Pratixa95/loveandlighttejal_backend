b687acd0ea864b207663012037f5a0a1fcf05663
Pratixa95 <mahetapratixa7695@gmail.com>
2026-03-16 13:45:14 +0530

    fixing enrollment again-again-again

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,14 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 import { env } from "./env.js";
 
+/* 🔥 IMPORT ALL SCHEMAS */
+import * as schema from "../db/schema/index.js";
+
 const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: env.databaseUrl,
 });
 
-export const db = drizzle(pool);
+/* ✅ PASS SCHEMA HERE (MOST IMPORTANT) */
+export const db = drizzle(pool, { schema });
