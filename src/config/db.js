8cabed630dc4f761117e472598c1dee0596fca65
Pratixa95 <mahetapratixa7695@gmail.com>
2026-04-09 14:15:27 +0530

    supabase pw change in env again

--- src/config/db.js
+++ src/config/db.js
@@ -1,15 +1,15 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 import { env } from "./env.js";
-
-/* 🔥 IMPORT ALL SCHEMAS */
 import * as schema from "../db/schema/index.js";
 
 const { Pool } = pg;
 
 export const pool = new Pool({
   connectionString: env.databaseUrl,
+  ssl: {
+    rejectUnauthorized: false, // required for Supabase
+  },
 });
 
-/* ✅ PASS SCHEMA HERE (MOST IMPORTANT) */
 export const db = drizzle(pool, { schema });
